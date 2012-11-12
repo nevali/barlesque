7c9675d5dc4c182fbdeff189b226f1f640955058
Mo McRoberts <mo@nevali.net>
2012-11-12 22:47:15 +0000

    Allow $page_type [body classes] to be specified, as well as $headExtra [contents transcluded into <head>]

--- templates/header.php
+++ templates/header.php
@@ -7,6 +7,8 @@ if(!isset($blq))
 }
 $blq->fetch();
 
+$page_type = 'blq ' . @$page_type;
+
 echo '<!DOCTYPE html>';
 echo '<html>';
 
@@ -14,9 +16,13 @@ echo '<head>';
 $this->title();
 echo $blq->head;
 $this->links();
+if(isset($headExtra))
+{
+	echo $headExtra;
+}
 echo '<script>bbccookies._showPrompt = function() { return false; }</script>';
 echo '</head>';
 
-echo '<body>';
+echo '<body class="' . _e($page_type) . '">';
 
 echo $blq->bodyfirst;
